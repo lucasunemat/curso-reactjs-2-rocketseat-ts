1580e91db0535ee1cbb1d40c22bc64e98bd0ca08
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-03 15:16:54 -0400

    aprendendo como colocar estilos globais na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 /**
  * Aqui você coloca o ThemeProvider como tag e usa theme como
  * parametro para passar o defaultTheme que você criou como
- * constante exportável em default.ts 
+ * constante exportável em default.ts;
+ * Nova aula:
+ * GlobalStyle importa os estilos globais que você configurou no arquivo global.ts
  */
 
 export function App() {
@@ -15,6 +18,8 @@ export function App() {
       <Button variant="secondary"/>
       <Button variant="sucesss" />
       <Button variant="danger" />
+
+      <GlobalStyle /> 
     </ThemeProvider>
   )
 }
