dbc34364847bef15d9c1ba07d9fed6369bc39d50
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-11 19:59:34 -0400

    estudando context api

--- src/home.tsx
+++ src/home.tsx
@@ -1,7 +1,41 @@
-export function Home() {
+import { createContext, useContext, useState } from 'react'
+
+const CycleContext = createContext({
+  activeCycle: 1, // valor inicial
+})
+
+export function Countdown() {
+  let { activeCycle } = useContext(CycleContext) // indicando que vou usar o contexto e pego uma var por destructuring
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <h1>
+      Countdown: {activeCycle}
+      <button
+        onClick={() => {
+          activeCycle = 4
+        }}
+      >
+        Alterar
+      </button>
+    </h1>
+  ) // e agora posso usar a variavel do contexto normalmente em qualquer lugar da aplicação
+}
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CycleContext)
+  return <h1>NewCycleForm: {activeCycle}</h1>
+}
+
+export function Home() {
+  // o estado precisa sempre ser colocado no componente mais por fora (componente pai), que
+  // envolve os componentes que precisam usar o estado
+  // veja que uso destrucutring para pegar o valor até quando preciso colocar na prop "value" (dúvida: activeCycle é o activeCycle do useState?)
+  const [activeCycle, setActiveCycle] = useState(0)
+  return (
+    <CycleContext.Provider value={{ activeCycle }}>
+      <div>
+        <Countdown />
+        <NewCycleForm />
+      </div>
+    </CycleContext.Provider>
   )
 }
