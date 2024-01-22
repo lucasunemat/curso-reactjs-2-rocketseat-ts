12e1cec5f6b227111ff701f84d4d44aa44de5900
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-22 00:06:44 -0400

    implementando nova lógica & switch no reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -60,29 +60,55 @@ export function CyclesContextProvider({
    */
   const [cyclesState, dispatch] = useReducer(
     (state: CycleState, action: any) => {
-      if (action.type === 'ADD_NEW_CYCLE') {
-        // retorna o estado atual + o novo ciclo (mesma regra que eu acionava com setCycles (state => [...state, newCycle]))
-        // esse return é o novo valor que queremos que o estado tenha
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle], // retorna os ciclos, mas adiciona o novo ciclo no final
-          activeCycleID: action.payload.newCycle.id, // ativa o ciclo recem criado colocando o id dele como atual
-        }
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          // retorna o estado atual + o novo ciclo (mesma regra que eu acionava com setCycles (state => [...state, newCycle]))
+          // esse return é o novo valor que queremos que o estado tenha
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle], // retorna os ciclos, mas adiciona o novo ciclo no final
+            activeCycleID: action.payload.newCycle.id, // ativa o ciclo recem criado colocando o id dele como atual
+          }
+        case 'INTERRUPT_CURRENT_CYCLE':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleID) {
+                return { ...cycle, interruptedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleID: null,
+          }
+
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleID) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+            activeCycleID: null,
+          }
+
+        default:
+          return state
       }
 
-      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleID) {
-              return { ...cycle, interruptedDate: new Date() }
-            } else {
-              return cycle
-            }
-          }),
-          activeCycleID: null,
-        }
-      }
+      // if (action.type === 'ADD_NEW_CYCLE') {
+      // }
+
+      // if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
+
+      // }
+
+      // if (action.type === 'MARK_CURRENT_CYCLE_AS_FINISHED') {
+
+      // }
 
       return state // se não for a ação que eu quero, retorna o estado atual
     },
